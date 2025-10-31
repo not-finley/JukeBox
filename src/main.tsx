b61aa9a2964532edf681e12c32f27d179b4dee6e
Finley <finley.harrison2019@gmail.com>
2025-10-31 15:59:13 -0400

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AuthProvider from '@/lib/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
+import { Analytics } from "@vercel/analytics/next";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <QueryProvider>
             <AuthProvider>
+                <Analytics/>
                 <App/>
             </AuthProvider>
         </QueryProvider>
