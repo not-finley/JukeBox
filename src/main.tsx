e0dd75eebde6c3def483a9af88b1c6b0a994f819
Finley <finley.harrison2019@gmail.com>
2025-10-31 16:03:46 -0400

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AuthProvider from '@/lib/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
