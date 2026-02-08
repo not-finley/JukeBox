bd099450b3c187178ab5821c8936f134beb50b0e
Finley Harrison <fharriso@uwaterloo.ca>
2026-02-08 18:01:49 -0500

    testing capacitor

---
+++ android/app/src/main/java/ca/jukeboxd/app/MainActivity.java
@@ -0,0 +1,5 @@
+package ca.jukeboxd.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ca.jukeboxd.app',
+  appName: 'jukebox',
+  webDir: 'dist'
+};
+
+export default config;
