cbda798d52abfef7bb4506a45bc9ff3dd8005a70
Finley Harrison <fharriso@uwaterloo.ca>
2026-02-22 20:32:17 -0500

    Update to add dynamic helmet test

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import AuthProvider from '@/lib/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
 import { Analytics } from "@vercel/analytics/react"
 import { PlayerProvider } from './context/PlayerContext';
+import { HelmetProvider } from 'react-helmet-async';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <AuthProvider>
                 <Analytics/>
                     <PlayerProvider>
+                        <HelmetProvider>
                         <App/>
+                        </HelmetProvider>
                     </PlayerProvider>
             </AuthProvider>
         </QueryProvider>
